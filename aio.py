5e1345be08ca346d604baa4840d753d0f9664d7d
Edbwrd <31602118+Transformerio@users.noreply.github.com>
2023-03-24 17:08:59 -0500

    Questions 1 -4 are all done. TODO: question5, Fix rangeof all the fetch functions so they're dynamic. Check FetchAuthorInfo description because writeCSV is reporting description values of less length.

--- aio.py
+++ aio.py
@@ -28,6 +28,27 @@ async def FetchBookInfo(url, session) -> dict:
             product_details[row.find("th").text] = row.find("td").text
         return product_details
 
+
+async def FetchAuthorInfo(author_name, url, session) -> dict:
+    # author name has to be passed by param becuase toscrape has a typo in its author-title tag where start:<h3> end </h2> via Google Chrome
+    # response.text() will report h3 including all the informatio in the author_container
+    async with session.get(url) as response:
+        authorSoup = BeautifulSoup(await response.text(), 'html.parser')
+        author_container = authorSoup.find('div', class_='author-details')
+        #author_name = author_container.findChil("h2", class_='author-title') decremented until toscrape fixes error
+        author_dob = author_container.find('span', class_='author-born-date').text
+        author_pob = author_container.find('span', class_='author-born-location').text
+        author_desc = author_container.find('div', class_='author-description').text
+        ##TODO: CHECK DESCRIPTION BECUASE CSV missing length
+        print("[FetchBookInfo] fetching {0}".format(len(author_desc.strip())))
+        product_details = {
+            "Author": author_name,
+            "Date of Birth": author_dob,
+            "Location of Birth": author_pob,
+            "Description": author_desc.strip()
+        }
+        return product_details
+
 async def FetchFiveStar():
     fivestar_href_List = []
     headers = [
@@ -113,21 +134,63 @@ def WriteToCSV(filename, header, data):
 
 
 async def FetchInspirationalQuotes():
-    quotes = []
-    async with aiohttp.ClientSession('http://quotes.toscrape.com/') as session:
-        for i in range(1,30):
-            async with session.get('/page/{0}/'.format(i)) as resp:
+    quote_details = []
+    header = [
+        'Quote',
+        'Author'
+    ]
+    async with aiohttp.ClientSession('https://quotes.toscrape.com/') as session:
+        for i in range(1,2):
+            async with session.get('/tag/inspirational/page/{0}/'.format(i)) as resp:
                 soup = BeautifulSoup(await resp.text(), 'html.parser')
-                print(i)
-                tags = soup.find_all('a', {'href': '/tag/inspirational/page/{0}/'.format(i)})
-                pprint(tags)
-
-
+                tags_parent = soup.find_all('div', class_="quote")
+                for item in tags_parent:
+                    quote_details.append(
+                        {
+                        'Quote': item.find('span', class_="text").text,
+                        'Author': item.find('small', class_="author").text
+                        }
+                    )
+    await session.close()
+    return header, quote_details
+    
+    
+async def FetchFriendshipQuotes():
+    author_detail_list = []
+    author_href = []
+    header = [
+        'Author',
+        'Date of Birth',
+        'Location of Birth',
+        'Description'
+    ]
+    tasks = []
+    async with aiohttp.ClientSession('https://quotes.toscrape.com/') as session:
+        for i in range(1,2):
+            async with session.get('/tag/friendship/page/{0}/'.format(i)) as resp:
+                soup = BeautifulSoup(await resp.text(), 'html.parser')
+                tags_parent = soup.find_all('div', class_="quote")
+                for quote in tags_parent:
+                    author_href.append(
+                        [
+                            quote.find('small', class_="author").text,
+                            quote.find_all('span')[1].find('a')['href'] #in form /author/[author name]
+                        ]) 
+                for href in author_href:
+                    task = asyncio.ensure_future(FetchAuthorInfo(href[0], href[1], session))
+                    tasks.append(task)
+                    
+                    author_detail_list = await asyncio.gather(*tasks)
+    await session.close()
+    return header, author_detail_list
+    
 
 async def main():
     #result = await FetchHistoricalFictionBooks()
-    test = await FetchInspirationalQuotes()
+    test = await FetchFriendshipQuotes()
+    pprint(test)
+    WriteToCSV("auhtor_data", test[0], test[1])
     #pprint(result[1])
     #WriteToCSV("asy", result[0], result[1])
 
-asyncio.run(main())
+asyncio.get_event_loop().run_until_complete(main())
